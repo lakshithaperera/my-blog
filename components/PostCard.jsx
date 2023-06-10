ff83166cf12701d6272ce7d632bda4bc9dc7b4c1
lakshithaperera <pasindulakshitha33@gmail.com>
2023-06-11 01:55:41 +0530

    8th comm

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -6,8 +6,8 @@ import Link from 'next/link';
 import { grpahCMSImageLoader } from '../util';
 
 const PostCard = ({ post }) => (
-  <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8 post-card">
-    {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
+  <div className="bg-white shadow-lg rounded-lg p-0 lg:p-5 pb-12 mb-8 post-card">
+    { <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
       <Image
         unoptimized
         loader={grpahCMSImageLoader}
@@ -16,15 +16,13 @@ const PostCard = ({ post }) => (
         layout="fill"
         src={post.featuredImage.url}
       />
-    </div> */}
-    <div className="relative overflow-hidden shadow-md pb-80 mb-6">
-      <img src={post.featuredImage.url} alt="" className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
-    </div>
+    </div> }
 
-    <h1 className="transition duration-700 text-center mb-8 cursor-pointer  text-3xl font-semibold">
+
+    <h1 className="transition duration-700 text-center mb-4 cursor-pointer  text-3xl font-semibold">
       <Link href={`/post/${post.slug}`}>{post.title}</Link>
     </h1>
-    <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
+    <div className="block lg:flex text-center items-center justify-center mb-4 w-full">
       <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
         <Image
           unoptimized
@@ -35,7 +33,7 @@ const PostCard = ({ post }) => (
           className="align-middle rounded-full"
           src={post.author.photo.url}
         />
-        <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
+        <p className="inline align-middle text-gray-700 ml-2 font-mediumg text-l">{post.author.name}</p>
       </div>
       <div className="font-medium text-gray-700">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -44,12 +42,12 @@ const PostCard = ({ post }) => (
         <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
       </div>
     </div>
-    <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+    <p className="text-center  font-normal px-4 lg:px-5 mb-5">
       {post.excerpt}
     </p>
     <div className="text-center">
       <Link href={`/post/${post.slug}`}>
-        <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continue Reading</span>
+        <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600  font-semibold rounded-full text-white px-8 py-3 cursor-pointer">Continue Reading</span>
       </Link>
     </div>
   </div>
