b38beba9660d7de153da11c630ec8a5df977198a
lakshithaperera <pasindulakshitha33@gmail.com>
2023-06-11 09:26:30 +0530

    17 comm

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Metadata } from 'next';
 import Header from './Header';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+
 
 const Layout = ({ children }) => (
   <>
-    <Header />
-    {children}
+    <html lang='en'>
+      <Header />
+      <body className={inter.className}>{children}</body>
+    </html>
   </>
 );
 
