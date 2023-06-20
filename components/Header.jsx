1fcbb59f7c4a6bec6bbbfd026ea2df847c0cef5c
lakshithaperera <pasindulakshitha33@gmail.com>
2023-06-20 18:39:30 +0530

    30 comm

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,12 +3,24 @@ import {BsFacebook} from "react-icons/bs"
 import {AiFillInstagram} from "react-icons/ai"
 import {FaTiktok} from "react-icons/fa"
 import {BsPinterest} from "react-icons/bs"
+import {RiMenuFoldFill} from "react-icons/ri"
+import {IoMdCloseCircle} from "react-icons/io"
 
 import Link from 'next/link';
 import { getCategories } from '../services';
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+    document.querySelector(".nav-item").style.display = "flex";
+  };
+
+  const handleClose = () => {
+    document.querySelector(".nav-item").style.display = "none";
+  }
 
   useEffect(() => {
     getCategories().then((newCategories) => {
@@ -26,10 +38,18 @@ const Header = () => {
         </div>
         <div className="hidden md:float-left md:contents nav-bar">
           <div className="nav-item">
+            <div className="menu-close-btn" onClick={handleClose}>
+              <IoMdCloseCircle className='menu-close-icon' />
+            </div>
           {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle  ml-4 font-semibold cursor-pointer">{category.name.split(' ').at(0)}</span></Link>
+                   <Link  key={index} href={`/category/${category.slug}`}>
+                    <span className="md:float-right mt-2 align-middle  ml-4 font-semibold cursor-pointer">{category.name.split(' ').at(0)}</span>
+                    </Link>
           ))}
           </div>
+           <div className="mobile-menu" onClick={handleToggle}>
+            <RiMenuFoldFill className='menu-icn'/>
+           </div>
              <div className="icons">
               <a href="/"><BsFacebook className='icon' /></a>
               <a href="/"><AiFillInstagram className='icon'/></a>
--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,11 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <title>Discount Delights: Your Ultimate Destination for Exclusive Affiliate Sales and Irresistible Deals!</title>
-          <meta property="og:title" content="Discount Delights: Your Ultimate Destination for Exclusive Affiliate Sales and Irresistible Deals!" key="title" />
+        <title>TechItems: Exploring the Latest Tech Items and Gadgets for Tech Enthusiasts</title>
+          <meta name="description" content="Discover the latest trends and in-depth insights on tech items and gadgets at TechItems. Our informative blog articles cover a wide range of topics, from reviews of the newest tech releases to expert guides on optimizing your tech devices. Stay updated and informed with TechItems, your go-to resource for all things tech" />
+          <meta property="og:title" content="TechItems: Exploring the Latest Tech Items and Gadgets for Tech Enthusiasts" key="title" />
+          <meta property="og:description" content="Discover the latest trends and in-depth insights on tech items and gadgets at TechItems. Our informative blog articles cover a wide range of topics, from reviews of the newest tech releases to expert guides on optimizing your tech devices. Stay updated and informed with TechItems, your go-to resource for all things tech" />
+          <meta property="og:url" content="https://www.techitems.online/" />
           <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> 
         </Head>
         <body>
