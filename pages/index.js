a1fbf3a6e98d08e24f45811143d9496a6652a59e
lakshithaperera <pasindulakshitha33@gmail.com>
2023-06-23 04:00:49 +0530

    34 comm

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
-import Footer from '../components/Footer';
+
 
 export default function Home({ posts }) {
   return (
-    <>
+    <section>
      <div className="container mx-auto px-5 md:px-10 mb-8">
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -24,7 +24,7 @@ export default function Home({ posts }) {
     </div>
  
 
-    </>
+    </section>
    
   );
 }
